8eda4e7a349d880da504626aca9d1302bf3a5d9f
Saso <msaso0725@gmail.com>
2026-09-22 22:01:43 +0200

    Masodik commit PM feladatot csinalgattam

---
+++ PMFeladat2/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PMFeladat2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqliteConnection connection;
+        private List<VedettAllat> vedettAllatok = new List<VedettAllat>();
+        private const string CONNECTION_STRING = "Data Source = vedett.db";
+        public MainWindow()
+        {
+            InitializeComponent();
+            establish_connection();
+        }
+        
+        private void establish_connection()
+        {
+            try 
+            {
+                connection = new SqliteConnection(CONNECTION_STRING);
+                
+            }
+            catch (Exception ex)
+            {
+                lb_connection_status.Content = "Status: Failed to connect";
+                MessageBox.Show($"Error: {ex.Message}");
+                return;
+            }
+            lb_connection_status.Content = "Status: Stable Connection";
+
+        }
+        private void dg_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void read_file()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select a file to read from";
+            
+            
+            if (ofd.ShowDialog() == null)
+                return;
+
+            string selectedFile = ofd.FileName;
+            load_data( selectedFile );
+        }
+        private void load_data(string selectedFile)
+        {
+            vedettAllatok = File.ReadAllLines(selectedFile).Select(_ => new VedettAllat(_)).ToList();
+        }
+    }
+}
---
+++ PMFeladat2/Models/VedettAllat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMFeladat2
+{
+    internal class VedettAllat
+    {
+        public VedettAllat(string sor)
+        {
+            string[] data = sor.Split(";");
+            this.nev = data[1];
+            this.ertek = int.Parse(data[2]);
+            this.since = DateTime.Parse(data[3]);
+            this.fajta = data[3];
+        }
+
+        public int id { get; set; }
+        public string nev { get; set; }
+        public int ertek { get; set; }
+        public DateTime since { get; set; }
+        public string fajta { get; set; }
+    }
+}
